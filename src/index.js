568fd6e3be062381c1c062f66376041346a2ad27
Ozkar Alvarez <ozkaralvarez98@hotmail.com>
2022-04-06 23:50:22 -0700

    before rebuild

--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ import compareAsc from "date-fns/compareAsc";
 export default class UI {
   static content = document.querySelector(".task-content");
   static taskPopup = document.querySelector(".task-popup");
-  static addPopUpBtn = document.querySelector(".add-task");
+  static addTaskPopUp = document.querySelector(".add-task");
   static todayTab = document.querySelector(".today-tab");
   static weekTab = document.querySelector(".week-tab");
+  static addProject = document.querySelector(".add-project");
+  static projectPopUp = document.querySelector(".project-popup");
+  static addProjectBtn = document.querySelector(".add-project-btn");
+  static projectList = document.querySelector(".project-list");
+  static cancelProjBtn = document.querySelector(".cancel-project");
 
   static contentDOM = new ListBinding(UI.content);
-
-  static addTaskToDOM(tasks) {
+  static toggleVisibility(element) {
+    element.classList.toggle("visibility");
+  }
+  static addTaskToDOM(tasks, color) {
     UI.contentDOM.listElement.innerHTML = "";
     for (let task of tasks) {
       let li = document.createElement("li");
@@ -22,6 +29,7 @@ export default class UI {
       let taskDate = document.createElement("div");
       let dateChange = document.createElement("input");
       li.classList.add("task-item");
+
       checkbox.classList.add("task-check");
       taskName.classList.add("task-name");
       taskDate.classList.add("task-date");
@@ -29,6 +37,10 @@ export default class UI {
       dateChange.classList.add("visibility");
       checkbox.setAttribute("type", "checkbox");
       dateChange.setAttribute("type", "date");
+
+      if (color !== "") {
+        li.style.borderLeft = `solid 10px ${task.color}`;
+      }
 
       taskName.textContent = task.name;
       taskDate.textContent = task.getFormattedDate();
@@ -40,40 +52,129 @@ export default class UI {
       UI.contentDOM.listElement.append(li);
     }
   }
+  static addProjectToDOM(projects) {
+    UI.projectList.innerHTML = "";
+    for (let proj of projects) {
+      let li = document.createElement("li");
+      let projName = document.createElement("div");
+      let deleteBtn = document.createElement("div");
+      li.classList.add("proj-item");
+      projName.classList.add("proj-name");
+      deleteBtn.classList.add("delete-icon");
+      deleteBtn.innerHTML = '<i class="fa-solid fa-xmark delete-proj"></i>';
 
-  static addToTaskData() {
+      projName.textContent = proj.name;
+
+      li.append(projName);
+      li.append(deleteBtn);
+      UI.projectList.append(li);
+    }
+  }
+
+  static addTaskToProj(project) {
     let nameInput = document.querySelector(".data-name");
     let dateInput = document.querySelector("#due-date");
+
     let task;
 
     if (dateInput.value === "") {
-      task = new Task(nameInput.value);
+      task = new Task(nameInput.value, "No date", project.color);
+      project.addTask(task);
+    } else {
+      task = new Task(nameInput.value, dateInput.value, project.color);
+      project.addTask(task);
+    }
+  }
+  static addToTaskData(color) {
+    let nameInput = document.querySelector(".data-name");
+    let dateInput = document.querySelector("#due-date");
+
+    let task;
+
+    if (dateInput.value === "") {
+      task = new Task(nameInput.value, "No date", color);
       UI.contentDOM.addTaskToArr(task);
     } else {
-      task = new Task(nameInput.value, dateInput.value);
+      task = new Task(nameInput.value, dateInput.value, color);
       UI.contentDOM.addTaskToArr(task);
     }
-    UI.addTaskToDOM(UI.contentDOM.taskData);
-    UI.updateTaskArr();
   }
+
   static updateTaskArr() {
     const listItems = UI.contentDOM.listElement.querySelectorAll("li");
+    const contentLabel = document.querySelector(".content-label");
+
     UI.contentDOM.taskData = [];
+
     listItems.forEach((e) => {
       let name = e.querySelector(".task-name");
       let date = e.querySelector(".task-date");
+      let color;
       name = name.textContent;
       date = date.textContent;
-      UI.contentDOM.taskData.push(new Task(name, date));
+      UI.contentDOM.taskData.push(new Task(name, date, color));
     });
+  }
+  static deleteProject(event) {
+    if (
+      event.target.classList.contains("delete-proj") &&
+      confirm("Are you sure you want to delete this project?")
+    ) {
+      const projectName =
+        event.target.parentElement.parentElement.querySelector(".proj-name");
+
+      for (let i in UI.contentDOM.projData) {
+        if (UI.contentDOM.projData[i].name === projectName.textContent) {
+          UI.contentDOM.projData[i].taskArr.forEach((e) => {
+            for (let j in UI.contentDOM.taskData) {
+              if (UI.contentDOM.taskData[j].name === e.name) {
+                UI.contentDOM.taskData.splice(j, 1);
+              }
+            }
+          });
+          UI.contentDOM.projData.splice(i, 1);
+        }
+      }
+      event.target.parentElement.parentElement.remove();
+      UI.loadHomePage();
+    }
+  }
+  static displayProjectTasks(event) {
+    UI.appendAddTaskBtn();
+    const addTaskBtn = document.querySelector(".add-task");
+    const projectName = event.target.textContent;
+    const contentLabel = document.querySelector(".content-label");
+    const taskPopUp = document.querySelector(".task-popup");
+    const projects = UI.contentDOM.projData;
+    if (event.target.nodeName !== "UL" && event.target.nodeName !== "I") {
+      contentLabel.textContent = "";
+      contentLabel.textContent = projectName;
+      if (!UI.taskPopup.classList.contains("visibility")) {
+        UI.toggleVisibility(UI.taskPopup);
+      }
+      addTaskBtn.addEventListener("click", () => {
+        UI.toggleVisibility(addTaskBtn);
+        UI.toggleVisibility(taskPopUp);
+      });
+      projects.forEach((e) => {
+        if (e.name === projectName) {
+          UI.addTaskToDOM(e.taskArr, e.color);
+        }
+      });
+    }
   }
   static removeTask(event) {
     const contentLabel = document.querySelector(".content-label");
+    let color;
     if (event.target.type === "checkbox") {
-      // event.target.parentElement.remove();
-
       const taskName = event.target.parentElement.querySelector(".task-name");
-
+      for (let proj of UI.contentDOM.projData) {
+        for (let i in proj.taskArr) {
+          if (taskName.textContent === proj.taskArr[i].name) {
+            proj.taskArr.splice(i, 1);
+          }
+        }
+      }
       for (let i in UI.contentDOM.taskData) {
         if (taskName.textContent === UI.contentDOM.taskData[i].name) {
           UI.contentDOM.taskData.splice(i, 1);
@@ -90,18 +191,40 @@ export default class UI {
         }
       }
     } else return;
+    for (let proj of UI.contentDOM.projData) {
+      if (contentLabel === proj.name) {
+        color = proj.color;
+      }
+    }
     if (contentLabel.textContent === "Home") {
-      UI.addTaskToDOM(UI.contentDOM.taskData);
+      UI.addTaskToDOM(UI.contentDOM.taskData, color);
     } else if (contentLabel.textContent === "Today") {
-      UI.addTaskToDOM(UI.contentDOM.todayData);
+      UI.addTaskToDOM(UI.contentDOM.todayData, color);
     } else if (contentLabel.textContent === "This Week") {
-      UI.addTaskToDOM(UI.contentDOM.weekData);
+      UI.addTaskToDOM(UI.contentDOM.weekData, color);
+    } else {
+      const contentLabel = document.querySelector(".content-label").textContent;
+
+      for (let proj of UI.contentDOM.projData) {
+        if (contentLabel === proj.name) {
+          color = proj.color;
+        }
+      }
+      const projTaskArr = UI.contentDOM.projData;
+      projTaskArr.forEach((e) => {
+        if (contentLabel === e.name) {
+          UI.addTaskToDOM(e.taskArr, color);
+        }
+      });
     }
   }
+
   static updateDate(event) {
     if (event.target.classList.value === "task-date") {
       let taskDate = event.target.parentElement.querySelector(".task-date");
       let dateChange = event.target.parentElement.querySelector(".date-change");
+      let taskName = event.target.parentElement.querySelector(".task-name");
+      let contentLabel = document.querySelector(".content-label");
 
       UI.toggleVisibility(taskDate);
       UI.toggleVisibility(dateChange);
@@ -114,15 +237,58 @@ export default class UI {
         const newDate = `${month}/${day}/${year}`;
 
         taskDate.textContent = newDate;
-        UI.updateTaskArr();
-        UI.contentDOM.filterTodayTasks();
-        UI.contentDOM.filterWeekTasks();
+        if (
+          contentLabel.textContent === "Home" ||
+          contentLabel.textContent === "Week" ||
+          contentLabel.textContent === "Today"
+        ) {
+          UI.updateTaskArr();
+          UI.contentDOM.filterTodayTasks();
+          UI.contentDOM.filterWeekTasks();
+          for (let proj of UI.contentDOM.projData) {
+            for (let i in proj.taskArr) {
+              if (taskName.textContent === proj.taskArr[i].name) {
+                proj.taskArr[i].setFormattedDate(dateChange.value);
+              }
+            }
+          }
 
-        e.stopImmediatePropagation();
+          e.stopImmediatePropagation();
+        } else {
+          for (let proj of UI.contentDOM.projData) {
+            for (let i in proj.taskArr) {
+              if (taskName.textContent === proj.taskArr[i].name) {
+                proj.taskArr[i].setFormattedDate(dateChange.value);
+                UI.contentDOM.taskData.forEach((e) => {
+                  if (taskName.textContent === e.name) {
+                    e.setFormattedDate(dateChange.value);
+                    UI.contentDOM.filterTodayTasks();
+                    UI.contentDOM.filterWeekTasks();
+                  }
+                });
+              }
+            }
+          }
+        }
       });
     }
   }
+  static toggleProjectPopUp() {
+    UI.toggleVisibility(UI.addProject);
+    UI.toggleVisibility(UI.projectPopUp);
+  }
+  static submitNewProject() {
+    const projectInput = document.querySelector(".project-input");
+    const projColor = document.querySelector(".proj-color");
+    UI.contentDOM.addProjToArr(
+      new Project(projectInput.value, projColor.value)
+    );
 
+    UI.toggleProjectPopUp();
+
+    UI.addProjectToDOM(UI.contentDOM.projData);
+    projectInput.value = "";
+  }
   static initBtnEvents() {
     const submitTaskBtn = document.querySelector(".add-btn");
     const cancelTaskBtn = document.querySelector(".cancel-btn");
@@ -130,14 +296,21 @@ export default class UI {
 
     UI.contentDOM.listElement.addEventListener("click", UI.removeTask);
     UI.contentDOM.listElement.addEventListener("click", UI.updateDate);
-
+    UI.projectList.addEventListener("click", UI.displayProjectTasks);
+    UI.projectList.addEventListener("click", UI.deleteProject);
     homeTab.addEventListener("click", UI.loadHomePage);
     UI.todayTab.addEventListener("click", UI.loadTodayPage);
     UI.weekTab.addEventListener("click", UI.loadWeekPage);
-
-    UI.addPopUpBtn.addEventListener("click", () => {
+    UI.addProject.addEventListener("click", UI.toggleProjectPopUp);
+    UI.cancelProjBtn.addEventListener("click", () => {
+      document.querySelector(".project-input").value = "";
+      UI.toggleVisibility(UI.projectPopUp);
+      UI.toggleVisibility(UI.addProject);
+    });
+    UI.addProjectBtn.addEventListener("click", UI.submitNewProject);
+    UI.addTaskPopUp.addEventListener("click", () => {
       UI.toggleVisibility(this.taskPopup);
-      UI.toggleVisibility(this.addPopUpBtn);
+      UI.toggleVisibility(this.addTaskPopUp);
     });
     cancelTaskBtn.addEventListener("click", () => {
       const addTaskCont = document.querySelector(".append-btn");
@@ -147,22 +320,36 @@ export default class UI {
       }
 
       UI.toggleVisibility(this.taskPopup);
-      UI.toggleVisibility(this.addPopUpBtn);
       document.querySelector(".data-name").value = "";
       document.querySelector("#due-date").value = "";
     });
     submitTaskBtn.addEventListener("click", () => {
       const addTaskCont = document.querySelector(".append-btn");
+      const contentLabel = document.querySelector(".content-label");
+      let color;
 
       if (!addTaskCont.classList.contains("visibility")) {
         UI.toggleVisibility(addTaskCont.querySelector(".add-task"));
       }
+      for (let proj of UI.contentDOM.projData) {
+        if (contentLabel.textContent === proj.name) {
+          color = proj.color;
+        }
+      }
+      UI.addToTaskData(color);
+      UI.addTaskToDOM(UI.contentDOM.taskData, color);
+      UI.updateTaskArr();
 
-      UI.addToTaskData();
+      for (let proj of UI.contentDOM.projData) {
+        if (proj.name === contentLabel.textContent) {
+          UI.addTaskToProj(proj);
+          UI.addTaskToDOM(proj.taskArr, proj.color);
+        }
+      }
+
       UI.contentDOM.filterTodayTasks();
       UI.contentDOM.filterWeekTasks();
-      UI.toggleVisibility(this.taskPopup);
-      UI.toggleVisibility(this.addPopUpBtn);
+      UI.toggleVisibility(UI.taskPopup);
       document.querySelector(".data-name").value = "";
       document.querySelector("#due-date").value = "";
     });
@@ -175,21 +362,20 @@ export default class UI {
     btn.textContent = "+ Add Task";
     btnContainer.append(btn);
   }
-  static toggleVisibility(element) {
-    element.classList.toggle("visibility");
-  }
-
-  // fix "+ add task" visibility issue
   static loadHomePage() {
     const contentLabel = document.querySelector(".content-label");
     UI.appendAddTaskBtn();
     const addTask = document.querySelector(".add-task");
+    if (!UI.taskPopup.classList.contains("visibility")) {
+      UI.toggleVisibility(UI.taskPopup);
+    }
     addTask.addEventListener("click", () => {
       UI.toggleVisibility(UI.taskPopup);
       UI.toggleVisibility(addTask);
     });
     contentLabel.textContent = "Home";
     UI.contentDOM.listElement.innerHTML = "";
+
     UI.addTaskToDOM(UI.contentDOM.taskData);
   }
   static loadTodayPage() {
@@ -197,6 +383,9 @@ export default class UI {
     const btnContainer = document.querySelector(".append-btn");
     btnContainer.innerHTML = "";
     contentLabel.textContent = "Today";
+    if (!UI.taskPopup.classList.contains("visibility")) {
+      UI.toggleVisibility(UI.taskPopup);
+    }
     UI.contentDOM.listElement.innerHTML = "";
 
     UI.addTaskToDOM(UI.contentDOM.todayData);
@@ -206,6 +395,9 @@ export default class UI {
     const btnContainer = document.querySelector(".append-btn");
     btnContainer.innerHTML = "";
     contentLabel.textContent = "This Week";
+    if (!UI.taskPopup.classList.contains("visibility")) {
+      UI.toggleVisibility(UI.taskPopup);
+    }
     UI.contentDOM.listElement.innerHTML = "";
 
     UI.addTaskToDOM(UI.contentDOM.weekData);
